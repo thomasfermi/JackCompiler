4e1fa975a1c0548570f119ce8cbb0eaa3eddcf2c
thomasfermi <mario.theers@gmail.com>
2018-12-21 18:57:43 +0100

    Small refactoring

--- src/jack_parser.rs
+++ src/jack_parser.rs
@@ -10,36 +10,29 @@ pub fn parse_class(tokens : &Vec<Token>){
 
 
     if tokens[i] == Token::Keyword("class".to_string()) {
+        // class
         output += "<class>\n";
         i+=1;
         output += "  <keyword> class </keyword>\n";
 
-        if let Token::Identifier(s) = &tokens[i] {
-             output += &format!("  <identifier> {} </identifier>\n", s);
-        }
-        else {
-            panic!("This is not good :(");
-        }
-        i+=1;
+        // className
+        output += &parse_name(tokens, &mut i, 2);
 
-        if tokens[i] == Token::Symbol('{') {
-            output += "  <symbol> { </symbol>\n";
-        }
-        else {
-            panic!("This is not good :(");
-        }
-        i+=1;
+        // {
+        output += &parse_specific_symbol(tokens, &mut i, '{', 2);
 
-        // parse as many class variable declarations as you can
+
+        // classVarDec*
         while let Some(s_class_var_dec) = parse_class_var_dec(tokens, &mut i){
             output += &s_class_var_dec;
         }
 
-        // parse as many subroutine declarations as you can
+        // subRoutineDec*
         while let Some(s_subroutine_dec) = parse_subroutine_dec(tokens, &mut i){
             output += &s_subroutine_dec;
         }
 
+        // }
         /*
         if tokens[i] == Token::Symbol('}') {
             output += "  <symbol> } </symbol>\n";
@@ -72,37 +65,16 @@ fn parse_class_var_dec(tokens : &Vec<Token>, i : &mut usize) -> Option<String> {
     *i+=1;
 
     // type
-    match &tokens[*i] {
-        Token::Keyword(kw) => {
-            if kw == "int" || kw == "char" || kw == "boolean" {
-                output += &format!("    <keyword> {} </keyword>\n",kw);
-            } else {
-                panic!("Expected a type! Type has to be int, char, boolean, or class name!")
-            }
-        },
-        Token::Identifier(id) => output += &format!("    <identifier> {} </identifier>\n",id),
-        _ => panic!("Expected a type! Type has to be int, char, boolean, or class name!")
-    }
-    *i +=1;
+    output += &parse_type(tokens, i);
 
     // varName
-    if let Token::Identifier(id) = &tokens[*i] {
-        output += &format!("    <identifier> {} </identifier>\n",id);
-        *i+=1;
-    } else {
-        panic!("Expected a variable name here!");
-    }
+    output += &parse_name(tokens, i, 4);
 
     // (, varName)*
     while tokens[*i] == Token::Symbol(',') {
         output += "    <symbol> , </symbol>\n";
         *i += 1;
-        if let Token::Identifier(id) = &tokens[*i] {
-            output += &format!("    <identifier> {} </identifier>\n",id);
-            *i+=1;
-        } else {
-            panic!("Expected a variable name here!");
-        }
+        output += &parse_name(tokens, i, 4);
     }
 
     // ;
@@ -135,9 +107,70 @@ fn parse_subroutine_dec(tokens : &Vec<Token>, i : &mut usize) -> Option<String> 
     }
     *i+=1;
 
-    //
+    // ( 'void' | type), TODO: code reuse is bad
+     match &tokens[*i] {
+        Token::Keyword(kw) => {
+            if kw == "int" || kw == "char" || kw == "boolean" || kw == "void" {
+                output += &format!("    <keyword> {} </keyword>\n",kw);
+            } else {
+                panic!("Expected a type! Type has to be int, char, boolean, or class name!")
+            }
+        },
+        Token::Identifier(id) => output += &format!("    <identifier> {} </identifier>\n",id),
+        _ => panic!("Expected a type! Type has to be int, char, boolean, or class name!")
+    }
+    *i +=1;
+
+    // subRoutineName, TODO: code reuse is bad
+    if let Token::Identifier(id) = &tokens[*i] {
+        output += &format!("    <identifier> {} </identifier>\n",id);
+        *i+=1;
+    } else {
+        panic!("Expected a subRoutine name here!");
+    }
+
+    // ( parameterList )
+
+    // subRoutineBody
 
     output += "  </subroutineDec>\n";
 
     return Some(output);
+}
+
+fn parse_type(tokens : &Vec<Token>, i : &mut usize) -> String {
+    match &tokens[*i] {
+        Token::Keyword(kw) => {
+            if kw == "int" || kw == "char" || kw == "boolean" {
+                *i +=1;
+                format!("    <keyword> {} </keyword>\n",kw)
+            } else {
+                panic!("Expected a type! Type has to be int, char, boolean, or class name!")
+            }
+        },
+        Token::Identifier(id) => {
+            *i+=1;
+            format!("    <identifier> {} </identifier>\n",id)
+        },
+        _ => panic!("Expected a type! Type has to be int, char, boolean, or class name!")
+    }
+}
+
+fn parse_name(tokens : &Vec<Token>, i : &mut usize, indent : usize) -> String {
+    if let Token::Identifier(id) = &tokens[*i] {
+         *i+=1;
+         format!("{:indent$}<identifier> {id:} </identifier>\n", "", indent=indent, id=id)
+    } else {
+        panic!("Expected a name here!");
+    }
+}
+
+fn parse_specific_symbol(tokens : &Vec<Token>, i : &mut usize, c : char, indent : usize) -> String {
+    if tokens[*i] == Token::Symbol('{') {
+        *i+=1;
+        format!("{:indent$}<symbol> {symbol:} </symbol>\n", "", indent=indent, symbol=c)
+    }
+    else {
+        panic!("Expected {");
+    }
 }
